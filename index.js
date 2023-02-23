5cf603f22917d553d456e558a3a037a7553f6d1b
Mario Aguilar <marioaguilar740@gmail.com>
2023-02-23 16:17:24 +0100

    Web server installed and configured with Express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Welcome to Shiny Teeth");
+// Importamos la libreria de Express
+const express = require('express');
+
+// Instanciamos una clase de Express en una variable
+const server = express(); 
+
+// Declaramos el uso de JSON para procesar el body en peticiones recibidas
+server.use(express.json());
+
+// Importamos las rutas de nuestra API
+const routes = require('./routes');
+server.use('/api', routes); 
+
+// Variable donde almacenamos el puerto donde correrá el servicio
+const PORT = 3000;
+
+// Asigno un puerto a la conexión y mediante un callback le indico que me avise cuando se haya realizado.
+server.listen(PORT, () => {
+    console.log(`Server is live at localhost: ${PORT}`)
+});
---
+++ routes.js
@@ -0,0 +1,9 @@
+// Incluimos el paquete Express 
+const { Router } = require('express');
+const router = Router();
+
+// Aqui estamos creando nuestra ruta principal de la API
+router.get('/', (req, res) => res.send('Welcome to Shiny Teeth'));
+
+// Aqui se exporta la ruta para su uso en otros modulos
+module.exports = router;
