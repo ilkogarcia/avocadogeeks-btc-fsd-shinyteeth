91bdad62a98542f49127d1d566cb5d667806972e
ilko.garcia <ilko.garcia@gmail.com>
2023-02-26 00:14:02 +0100

    Added ESLint package for static code review find problems and fix. Some configuration is still necessary to fine tune the operation combined with Prettier

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 // Incluimos el paquete Express 
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
 // Aqui estamos creando nuestra ruta principal de la API
-router.get('/', (req, res) => res.send('Welcome to Shiny Teeth'));
+router.get("/", (req, res) => res.send("Welcome to Shiny Teeth"));
 
 // Aqui se exporta la ruta para su uso en otros modulos
 module.exports = router;
