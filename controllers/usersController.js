35e0d266866e2e203a293fc4c188eadbbfa09dc5
ilko.garcia <ilko.garcia@gmail.com>
2023-03-01 16:13:43 +0100

    feat: Users routes and controller OK

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,15 +6,15 @@ module.exports = class UserCtrl {
   static async apiAddUser (req, res) {
     try {
       const newUserData = {
-        role_id: req.role_id,
-        patient_id: req.patient_id,
-        professional_id: req.professional_id,
-        first_name: req.first_name,
-        middle_name: req.middle_name,
-        last_name: req.last_name,
-        mobile_phone: req.mobile_phone,
-        email: req.email,
-        password_hash: req.password_hash
+        role_id: req.body.role_id,
+        patient_id: req.body.patient_id,
+        professional_id: req.body.professional_id,
+        first_name: req.body.first_name,
+        middle_name: req.body.middle_name,
+        last_name: req.body.last_name,
+        mobile_phone: req.body.mobile_phone,
+        email: req.body.email,
+        password_hash: req.body.password_hash
       }
       const response = await tbl_User.create(newUserData)
       return res.json(response)
@@ -26,19 +26,17 @@ module.exports = class UserCtrl {
   // CRUD: (U) Update a user record in the database. Data passed in body request
   static async apiUpdateUser (req, res) {
     try {
-      const userId = req.params.id
-      const newUserData = {
-        role_id: req.role_id,
-        patient_id: req.patient_id,
-        professional_id: req.professional_id,
-        first_name: req.first_name,
-        middle_name: req.middle_name,
-        last_name: req.last_name,
-        mobile_phone: req.mobile_phone,
-        email: req.email,
-        password_hash: req.password_hash
-      }
-      const response = await tbl_User.update({ newUserData }, { where: { id: userId } })
+      const response = await tbl_User.update({
+        role_id: req.body.role_id,
+        patient_id: req.body.patient_id,
+        professional_id: req.body.professional_id,
+        first_name: req.body.first_name,
+        middle_name: req.body.middle_name,
+        last_name: req.body.last_name,
+        mobile_phone: req.body.mobile_phone,
+        email: req.body.email,
+        password_hash: req.body.password_hash
+      }, { where: { id: req.params.id } })
       return res.json(response)
     } catch (error) {
       return res.status(500).json({ error })
@@ -48,12 +46,11 @@ module.exports = class UserCtrl {
   // CRUD: (R) Retrive user data from database. The user ID received in request parameter
   static async apiGetUserById (req, res) {
     try {
-      const userId = req.params.id
-      const userProfile = await tbl_User.findByPk(userId)
-      if (!userProfile) {
+      const response = await tbl_User.findByPk(req.params.id)
+      if (!response) {
         return res.status(404).json('¡Este usuarios no existe en la base de datos!')
       } else {
-        return res.json(userProfile)
+        return res.json(response)
       }
     } catch (error) {
       return res.status(500).json({ error })
@@ -63,19 +60,18 @@ module.exports = class UserCtrl {
   // CRUD: (D) Delete from database the user record. The user ID received in request parameter
   static async apiDeleteUser (req, res) {
     try {
-      const userId = req.params.id
-      const userProfile = await tbl_User.destroy({ where: { id: userId } })
-      if (!userProfile) {
+      const response = await tbl_User.destroy({ where: { id: req.params.id } })
+      if (!response) {
         return res.status(404).json('¡Este usuarios no existe en la base de datos!')
       } else {
-        return res.json(userProfile)
+        return res.json(response)
       }
     } catch (error) {
       return res.status(500).json({ error })
     }
   }
 
-  // Just for testing... NOT SECURE METHOD... Remove
+  // This method is available only in the backend and for users with administration privileges
   static async apiGetAllUsers (req, res) {
     try {
       const usersList = await tbl_User.findAll()
--- routes/routes.users.js
+++ routes/routes.users.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const UserCtrl = require('../controllers/usersController')
 // const verifyToken = require("../middlewares/verifyToken");
 
-router.get('/users', UserCtrl.apiGetAllUsers)
-router.get('/users/:id', UserCtrl.apiGetUserById)
-router.delete('/users/:id', UserCtrl.apiDeleteUser)
-router.post('/users', UserCtrl.apiAddUser)
-router.put('/users/:id', UserCtrl.apiUpdateUser)
+router.post('/users', UserCtrl.apiAddUser) // CRUD: (C) Create
+router.get('/users/:id', UserCtrl.apiGetUserById) // CRUD: (R) Retrieve
+router.put('/users/:id', UserCtrl.apiUpdateUser) // CRUD: (U) Update
+router.delete('/users/:id', UserCtrl.apiDeleteUser) // CRUD: (D) Delete
+router.get('/users', UserCtrl.apiGetAllUsers) // Not secure... remove?
 
 module.exports = router
