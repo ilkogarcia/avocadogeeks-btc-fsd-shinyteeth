0dc13dc02baac131ce489735330d675bc55b7462
ilko.garcia <ilko.garcia@gmail.com>
2023-03-01 23:12:23 +0100

    feat: JSON Web Token (JWT) authentication implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,12 @@
 /* eslint-disable camelcase */
 
-// Import bcrypt, a password-hashing function based on the Blowfish cipher
+// Import .env file using commonjs
+require('dotenv').config()
+
+// Import bcrypt package with a password-hashing function based on the Blowfish cipher
 const bcrypt = require('bcrypt')
+// Import jsonwebtoken (JWTs) package to support authorization and information exchange
+const jwt = require('jsonwebtoken')
 // Import Sequelize models used
 const { tbl_User } = require('../models/index')
 
@@ -16,12 +21,22 @@ module.exports = class AuthCtrl {
       const password_hash = bcrypt.hashSync(password, 10)
 
       // Create new user register in database
-      const response = await tbl_User.create({
+      const user = await tbl_User.create({
         email,
         password_hash
       })
 
-      return res.status(201).json({ message: 'User signup successfully.', response })
+      if (!user) {
+        return res.status(400).json({ message: 'Something has gone wrong!' })
+      }
+
+      // We put together the answer that we are going to return
+      const response = {
+        message: 'User signup successfully!',
+        userId: user.id
+      }
+
+      return res.status(201).json({ response })
     } catch (error) {
       return res.status(500).json({ error })
     }
@@ -34,16 +49,35 @@ module.exports = class AuthCtrl {
       const { email, password } = req.body
 
       // Find user with requested email
-      const response = await tbl_User.findOne({ where: { email } })
-      if (!response) {
+      const user = await tbl_User.findOne({ where: { email } })
+      if (!user) {
         return res.status(400).json({ message: 'Something has gone wrong!. Try again' })
       } else {
-        const isMatch = bcrypt.compareSync(password, response.password_hash)
+        const isMatch = bcrypt.compareSync(password, user.password_hash)
         if (!isMatch) {
           return res.status(400).json({ message: 'Something has gone wrong!. Try again' })
         }
       }
-      return res.status(201).json({ message: 'Login success. Access granted!!!', response })
+
+      // Generate a web token with default (HMAC SHA256)
+      const token = jwt.sign(
+        {
+          userId: user.id,
+          email: user.email,
+          roleId: user.role_id
+        },
+        process.env.SECRET,
+        { expiresIn: '1h' }
+      )
+
+      // We put together the answer that we are going to return
+      const response = {
+        message: 'Access granted!',
+        userID: user.id,
+        sessionToken: token
+      }
+
+      return res.status(201).json({ response })
     } catch (error) {
       return res.status(500).send(error.message)
     }
