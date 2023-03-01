5c5cb5316592b886875ff11b8c1287ea71103676
ilko.garcia <ilko.garcia@gmail.com>
2023-03-01 20:51:09 +0100

    build: jsonwebtoken package added to the project

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,13 +9,19 @@ module.exports = class AuthCtrl {
   // Authentication API: SignUp
   static async apiSignUp (req, res) {
     try {
+      // Get user email and password from body request
       const { email, password } = req.body
+
+      // Generate password HASH using bCrypt method
       const password_hash = bcrypt.hashSync(password, 10)
+
+      // Create new user register in database
       const response = await tbl_User.create({
         email,
         password_hash
       })
-      return res.json(response)
+
+      return res.status(201).json({ message: 'User signup successfully.', response })
     } catch (error) {
       return res.status(500).json({ error })
     }
@@ -24,17 +30,20 @@ module.exports = class AuthCtrl {
   // Authentication API: SignIn
   static async apiSignIn (req, res) {
     try {
+      // Get user email and password from body request
       const { email, password } = req.body
-      const user = await tbl_User.findOne({ where: { email } })
-      if (!user) {
-        return res.send('Something has gone wrong!. Try again')
+
+      // Find user with requested email
+      const response = await tbl_User.findOne({ where: { email } })
+      if (!response) {
+        return res.status(400).json({ message: 'Something has gone wrong!. Try again' })
       } else {
-        const isMatch = bcrypt.compareSync(password, user.password_hash)
+        const isMatch = bcrypt.compareSync(password, response.password_hash)
         if (!isMatch) {
-          return res.send('Something has gone wrong!. Try again')
+          return res.status(400).json({ message: 'Something has gone wrong!. Try again' })
         }
       }
-      return res.status(200).send('Login success. Access granted!!!')
+      return res.status(201).json({ message: 'Login success. Access granted!!!', response })
     } catch (error) {
       return res.status(500).send(error.message)
     }
