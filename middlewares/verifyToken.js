4756c8ffa869d407a61beb18f602a9f5fcb28235
ilko.garcia <ilko.garcia@gmail.com>
2023-03-02 20:08:55 +0100

    refactor: Verify token function. Better treatment of 401 responses. Personalized messages according to the response of the JWT verification method

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,18 +1,27 @@
+/*
+* Security middleware for authentication using jsonwebtoken.
+* We add a middleware function to validate a JSON Web Token (JWT)
+* and return a 401 Unauthorized error if not.
+*/
+
 /* eslint-disable camelcase */
 
-// Import .env file using commonjs
+// We import the .env file that contains all the global variables of the project
 require('dotenv').config()
 
-// Import jsonwebtoken (JWTs) package to support authorization and information exchange
+// Import jsonwebtoken (JWTs) package that support authorization and information exchange
 const jwt = require('jsonwebtoken')
 
-// Verify token middleware function
+// Middleware function that validate a JSON Web Token (JWT)
 const verifyToken = (req, res, next) => {
   try {
     // Retrieve token from the request header
     const authorization = req.headers.authorization
     if (!authorization) {
-      return res.status(400).json({ message: 'Invalid token!' })
+      return res.status(401).json({
+        sucess: false,
+        message: 'Unauthorized!'
+      })
     }
 
     // eslint-disable-next-line no-unused-vars
@@ -27,7 +36,26 @@ const verifyToken = (req, res, next) => {
 
     next()
   } catch (error) {
-    return res.status(500).send(error.message)
+    switch (error.name) {
+      case 'TokenExpiredError':
+        return res.status(401).json({
+          sucess: false,
+          message: 'Unauthorized! - Token is expired',
+          expiredAt: error.expiredAt
+        })
+      case 'NotBeforeError':
+        return res.status(401).json({
+          sucess: false,
+          message: 'Unauthorized! - Token is not active',
+          date: error.date
+        })
+      default:
+        return res.status(401).json({
+          sucess: false,
+          message: 'Unauthorized! - Token error',
+          error: error.message
+        })
+    }
   }
 }
 
