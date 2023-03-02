473856245e42057159fe17ccc2cb75e23136032f
ilko.garcia <ilko.garcia@gmail.com>
2023-03-02 21:49:50 +0100

    feat: Completed a preview version of the isPatient middleware

--- routes/routes.patient.js
+++ routes/routes.patient.js
@@ -1,8 +1,15 @@
+// Import Express package
 const express = require('express')
 const router = express.Router()
+
+// Import Patient controller
 const PatientCtrl = require('../controllers/patientsControllers')
 
+// Import middleware functions
+const verifyToken = require('../middlewares/verifyToken')
+const isPatient = require('../middlewares/isPatient')
+
 // router.post('/patient', PatientCtrl.apiAddPatient)
-router.get('/patient', PatientCtrl.apiGetAllPatient)
+router.get('/patient', verifyToken, isPatient, PatientCtrl.apiGetAllPatient)
 
 module.exports = router
