9479b5a83f3ef4ca323f57494babd3c160585bde
ilko.garcia <ilko.garcia@gmail.com>
2023-03-03 00:40:42 +0100

    Add middleware in patient and dental specialties routes

--- routes/routes.dentalspecialties.js
+++ routes/routes.dentalspecialties.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router()
 const DentSpeCtrl = require('../controllers/dentalspecialtiesControllers')
 const verifyToken = require('../middlewares/verifyToken')
+const isAdmin = require('../middlewares/isAdmin')
 
-router.post('/dentalspecialties', verifyToken, DentSpeCtrl.apiAddDentSpec)
-router.put('/dentalspecialties/:id', verifyToken, DentSpeCtrl.apiUpdateDentSpec)
-router.get('/dentalspecialties/:id', verifyToken, DentSpeCtrl.apiGetDentSpecById)
-router.delete('/dentalspecialties/:id', verifyToken, DentSpeCtrl.apiDeleteDentSpec)
+
+router.post('/dentalspecialties', verifyToken, isAdmin, DentSpeCtrl.apiAddDentSpec)
+router.put('/dentalspecialties/:id', verifyToken, isAdmin, DentSpeCtrl.apiUpdateDentSpec)
+router.get('/dentalspecialties/:id', verifyToken, isAdmin, DentSpeCtrl.apiGetDentSpecById)
+router.delete('/dentalspecialties/:id', verifyToken, isAdmin, DentSpeCtrl.apiDeleteDentSpec)
 router.get('/dentalspecialties', verifyToken, DentSpeCtrl.apiGetAllDentSpec)
 
 module.exports = router
--- routes/routes.patient.js
+++ routes/routes.patient.js
@@ -8,11 +8,13 @@ const PatientCtrl = require('../controllers/patientsControllers')
 // Import middleware functions
 const verifyToken = require('../middlewares/verifyToken')
 const isProfessional = require('../middlewares/isProfessional')
+const isAdmin = require('../middlewares/isAdmin')
+
 
 router.post('/patient', verifyToken, isProfessional, PatientCtrl.apiAddPatient)
 router.get('/patient/:id', verifyToken, PatientCtrl.apiGetPatientById)
 router.put('/patient/:id', verifyToken, isProfessional, PatientCtrl.apiUpdatePatient)
-router.delete('/patient/:id', verifyToken, isProfessional, PatientCtrl.apiDeletePatient)
-router.get('/patient', verifyToken, isProfessional, PatientCtrl.apiGetAllPatient)
+router.delete('/patient/:id', verifyToken, isAdmin, PatientCtrl.apiDeletePatient)
+router.get('/patient', verifyToken, isAdmin, PatientCtrl.apiGetAllPatient)
 
 module.exports = router
