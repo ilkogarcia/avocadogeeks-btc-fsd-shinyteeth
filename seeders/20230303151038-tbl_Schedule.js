6e731576bc82da77f0ce88a9e30b6e0eb5d8d114
ilko.garcia <ilko.garcia@gmail.com>
2023-03-03 17:57:35 +0100

    feat: New seed for tbl_Schedule table

--- seeders/20230303151038-tbl_Schedule.js
+++ seeders/20230303151038-tbl_Schedule.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('tbl_Schedules', [
+      { profesional_id: 8, appointment_id: 4, workday: '2023-03-05', start_time_slot: '09:00:00', end_time_slot: '09:45:00', available: false, createdAt: new Date(), updatedAt: new Date() },
+      { profesional_id: 6, appointment_id: 5, workday: '2023-03-06', start_time_slot: '09:00:00', end_time_slot: '09:45:00', available: false, createdAt: new Date(), updatedAt: new Date() },
+      { profesional_id: 3, appointment_id: 6, workday: '2023-03-06', start_time_slot: '10:00:00', end_time_slot: '10:45:00', available: false, createdAt: new Date(), updatedAt: new Date() },
+      { profesional_id: 3, appointment_id: 7, workday: '2023-03-10', start_time_slot: '15:00:00', end_time_slot: '15:45:00', available: false, createdAt: new Date(), updatedAt: new Date() },
+      { profesional_id: 8, appointment_id: 8, workday: '2023-03-11', start_time_slot: '18:30:00', end_time_slot: '19:45:00', available: false, createdAt: new Date(), updatedAt: new Date() },
+      { profesional_id: 6, appointment_id: 9, workday: '2023-03-05', start_time_slot: '10:00:00', end_time_slot: '10:45:00', available: false, createdAt: new Date(), updatedAt: new Date() },
+      { profesional_id: 6, appointment_id: 10, workday: '2023-03-10', start_time_slot: '09:00:00', end_time_slot: '09:45:00', available: false, createdAt: new Date(), updatedAt: new Date() }
+    ], {})
   },
 
   async down (queryInterface, Sequelize) {
@@ -22,4 +22,4 @@ module.exports = {
      * await queryInterface.bulkDelete('People', null, {});
      */
   }
-};
+}
